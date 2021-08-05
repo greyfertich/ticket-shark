e2ffbefa869769a8cfccd1b8e9a65e4318285bbe
Grey Fertich <greyfertich@gmail.com>
2021-08-05 16:22:33 -0700

    Initial Commit

---
+++ ticket-shark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ticket-shark
+//
+//  Created by Grey Fertich on 8/5/21.
+//  Copyright © 2021 Grey Fertich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
